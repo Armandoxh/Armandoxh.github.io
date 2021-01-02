966dbc7f44b6a8c922adda668c88d76e714b8ec3
Armando Xhimanki <armando.xhimanki@gmail.com>
2021-01-02 15:30:47 -0500

    basic gameboard and logic

--- Battleship-2.0/js/script.js
+++ Battleship-2.0/js/script.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+const gameRatio = 10; 
+const ships = {
+    Cruiser: {
+        length : 5 
+    },
+    aircraftCarrier : {
+        length : 2
+    }
+}
+
+const $fleetlocation = $('#fleetLocation');
+
+console.log($fleetlocation)
