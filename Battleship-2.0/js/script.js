7e4ce4641c5400b68c7869043744a86309a8f6eb
Armando Xhimanki <armando.xhimanki@gmail.com>
2021-01-02 16:28:17 -0500

    add elements dynamically to board

--- Battleship-2.0/js/script.js
+++ Battleship-2.0/js/script.js
@@ -1,4 +1,6 @@
-const gameRatio = 10; 
+const gameRatioX = 5; 
+const gameRatioY = 5;
+
 const ships = {
     Cruiser: {
         length : 5 
@@ -7,7 +9,24 @@ const ships = {
         length : 2
     }
 }
+const cell = `<div class="col border"> X  </div>`
 
 const $fleetlocation = $('#fleetLocation');
+
+for (let i = 0; i < gameRatioX; i++) {  
+    $fleetlocation.append(`<div id = "row${i}" class="row row-cols-5" style="height: 100px; 
+    background-color: rgba(20, 9, 9, 0.1);">
+    `)
+    $curentRow = $fleetlocation.find('#row0')
+    console.log($currentRow)
+
+        for (let j = 0; j < gameRatioY; j++) {
+
+            $currentRow.append(cell)
+}
+    
+}
+
+
 
 console.log($fleetlocation)
