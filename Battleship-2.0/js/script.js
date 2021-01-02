480bab627ee81a91c631785784de31e6aaab7394
Armando Xhimanki <armando.xhimanki@gmail.com>
2021-01-02 17:31:23 -0500

    initial display 50%

--- Battleship-2.0/js/script.js
+++ Battleship-2.0/js/script.js
@@ -1,36 +1,40 @@
 const gameRatioX = 5; 
 const gameRatioY = 5;
-const $fleetlocation = $('#fleetLocation');
-
+const $hitLocation = $('#hitLocation');
+const playerBoard = {
+    board : []
+}
 
 const ships = {
     Cruiser: {
         length : 5 
     },
+
     aircraftCarrier : {
         length : 2
     }
 }
-const cell = `<div class="col border"> X </div>`
 
+const populateEmptyBoard = function(){
 
-for (let i = 0; i < gameRatioX; i++) {  
+    for (let i = 0; i < gameRatioX; i++) {  
+        /**
+         * Must change cols to match gameratios "row-cols-X" when the game gets larger
+         */
+        $hitLocation.append(`<div id = "row${i}" class="row row-cols-5 border" style="height: 60px; width:250px;
+        background-color: rgba(140, 9, 9, 0.1);">
+        `)
+        $currentRow = $hitLocation.find('#row'+ i);
+        
+        playerBoard.board[i]= []
     
-    $fleetlocation.append(`<div id = "row${i}" class="row row-cols-5" style="height: 100px; 
-    background-color: rgba(20, 9, 9, 0.1);">
-    `)
-
-    $currentRow = $fleetlocation.find('#row'+ i)
-
-    console.log($currentRow)
-
-        for (let j = 0; j < gameRatioY; j++) {
-
-            $currentRow.append(cell)
-}
-    
+            for (let j = 0; j < gameRatioY; j++) {
+                $currentRow.append(`<div id = "${i}${j}" class="col border-end "> ${i}${j} </div>`)
+                console.log($currentRow.find("#ij"))
+                playerBoard.board[i][j]= 0;
+            }
+    }
 }
 
-
-
-console.log($fleetlocation)
+populateEmptyBoard();
+console.log(playerBoard)
