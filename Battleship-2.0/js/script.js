8a1a0fee2705a16fabf8e2f478cd76dac934d4a9
Armando Xhimanki <armando.xhimanki@gmail.com>
2021-01-02 18:53:28 -0500

    display is looking nicer

--- Battleship-2.0/js/script.js
+++ Battleship-2.0/js/script.js
@@ -1,6 +1,10 @@
 const gameRatioX = 5; 
 const gameRatioY = 5;
 const $hitLocation = $('#hitLocation');
+const $shipLocation= $('#shipLocation');
+console.log($shipLocation)
+
+
 const playerBoard = {
     board : []
 }
@@ -15,26 +19,47 @@ const ships = {
     }
 }
 
-const populateEmptyBoard = function(){
+
+
+const populateEmptyShipBoard = function(){
+    for (let i = 0; i < gameRatioX; i++) {  
+        /**
+         * Must change cols to match gameratios "row-cols-X" when the game gets larger
+         */
+        $shipLocation.append(`<div id = "row${i}" class="row row-cols-5 border" style="height: 100px; width:450px;
+        background-color: rgba(140, 9, 9, 0.1);">
+        `)
+        $currentRow = $shipLocation.find('#row'+ i);
+        
+            for (let j = 0; j < gameRatioY; j++) {
+                $currentRow.append(`<div id = "${i}${j}" class="col border-end border-top "> ewrq </div>`)
+                
+               
+            }
+    }
+
+}
+const populateEmptyAttackBoard = function(){
 
     for (let i = 0; i < gameRatioX; i++) {  
         /**
          * Must change cols to match gameratios "row-cols-X" when the game gets larger
          */
-        $hitLocation.append(`<div id = "row${i}" class="row row-cols-5 border" style="height: 60px; width:250px;
-        background-color: rgba(140, 9, 9, 0.1);">
+        $hitLocation.append(`<div id = "row${i}" class="row row-cols-5 border" style="height: 100px; width:450px;
+        background-color: rgba(140, 9, 9, 0.1); ">
         `)
         $currentRow = $hitLocation.find('#row'+ i);
         
         playerBoard.board[i]= []
     
             for (let j = 0; j < gameRatioY; j++) {
-                $currentRow.append(`<div id = "${i}${j}" class="col border-end "> ${i}${j} </div>`)
-                console.log($currentRow.find("#ij"))
+                $currentRow.append(`<div id = "${i}${j}" class="col border-end border-top "> ${i}${j} </div>`)
+                
                 playerBoard.board[i][j]= 0;
             }
     }
 }
 
-populateEmptyBoard();
+populateEmptyShipBoard();
+populateEmptyAttackBoard();
 console.log(playerBoard)
